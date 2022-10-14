7e3fcdf50e80ce892f280d47c9da975a33e1a2c9
PKoding_World <92498953+Pklee93@users.noreply.github.com>
2022-10-13 23:37:28 -0400

    Add files via upload

---
+++ Oct_13_2022/src/Solution.java
@@ -0,0 +1,48 @@
+/*
+ * Given the heads of two sorted linked lists
+ * Merge the two lists in a one sorted list.
+ */
+
+class ListNode {
+	int val;
+	ListNode next;
+	
+	ListNode(){}
+	
+	ListNode(int val) {
+		this.val = val;
+	}
+	
+	ListNode(int val, ListNode next) {
+		this.val = val;
+		this.next = next;
+	}
+}
+
+public class Solution {
+	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+		if (list1 == null) {
+			return list2;
+		}
+		if (list2 == null) {
+			return list1;
+		}
+		if (list1.val < list2.val) {
+			list1.next = mergeTwoLists(list1.next, list2);
+			return list1;
+		} else {
+			list2.next = mergeTwoLists(list1, list2.next);
+			return list2;
+		}
+	}
+}
+
+/*
+ * Input: list1 = [], list2 = []
+ * Output: []
+ * 
+ * Input: list1 = [], list2 = [0]
+ * Output: []
+ * 
+ * 
+ */
